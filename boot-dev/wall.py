c83f686b5f9716e0e92c8a03ea0b6f03e2f4a018
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2023-05-20 21:41:25 +0100

    Creating a class called 'Wall'.

---
+++ boot-dev/wall.py
@@ -0,0 +1,14 @@
+'''Creating a class called Wall with a property called armor
+that is initialized to 10 and a height that starts at 5.
+'''
+
+class Wall:
+	armor = 10
+	height = 5
+
+def main():
+	wall = Wall()
+	print(wall.armor)
+	print(wall.height)
+
+main()
