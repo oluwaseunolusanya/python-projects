6a41c37edc8c84cbfbe6969f15f2c9205dbf8188
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2023-05-20 21:50:04 +0100

    Adding fortify method.

--- boot-dev/wall.py
+++ boot-dev/wall.py
@@ -5,10 +5,17 @@ that is initialized to 10 and a height that starts at 5.
 class Wall:
 	armor = 10
 	height = 5
+	
+	def fortify(self):
+		self.armor *= 2
 
 def main():
 	wall = Wall()
 	print(wall.armor)
 	print(wall.height)
+	wall.fortify()
+	print(wall.armor)
+	wall.fortify()
+	print(wall.armor)
 
 main()
