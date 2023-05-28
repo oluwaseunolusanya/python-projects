cbfec18325452af4afed039ea674e61b79d6ca5e
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-05-28 11:20:02 +0100

    Modified files.

--- boot-dev/wall.py
+++ boot-dev/wall.py
@@ -6,16 +6,34 @@ class Wall:
 	armor = 10
 	height = 5
 	
+	def get_cost(self):
+		return self.armor * self.height
+
 	def fortify(self):
 		self.armor *= 2
 
 def main():
-	wall = Wall()
-	print(wall.armor)
-	print(wall.height)
-	wall.fortify()
-	print(wall.armor)
-	wall.fortify()
-	print(wall.armor)
+    wall = Wall()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+
+    print("fortifying wall...")
+    wall.fortify()
+    print(f"Cost of wall: {wall.get_cost()}")
+    
+    wall_2 = Wall()
+    print(wall_2.armor)
+    wall_2.armor = 20
+    print(wall_2.armor)
+    Wall.armor = 5
+    print(wall_2.armor)    
+
 
 main()
